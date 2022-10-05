f7ce10fcf77c2d3cbb48c1cd04a9216675f12a99
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-10-05 17:43:46 +0200

    sequential + ui

--- ParallelProgramingCompulsory-Primes/Primes/PrimeGeneratorForm.Designer.cs
+++ ParallelProgramingCompulsory-Primes/Primes/PrimeGeneratorForm.Designer.cs
@@ -29,97 +29,136 @@
         private void InitializeComponent()
         {
             this.ListLabel = new System.Windows.Forms.Label();
-            this.CalculatePrimesParallel = new System.Windows.Forms.Button();
+            this.CalculatePrimesSequential = new System.Windows.Forms.Button();
             this.PrimesList = new System.Windows.Forms.ListBox();
-            this.fromInput = new System.Windows.Forms.TextBox();
             this.FromLabel = new System.Windows.Forms.Label();
             this.toLabel = new System.Windows.Forms.Label();
-            this.toInput = new System.Windows.Forms.TextBox();
+            this.CalculatePrimesParallel = new System.Windows.Forms.Button();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.fromInput = new System.Windows.Forms.NumericUpDown();
+            this.toInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.fromInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toInput)).BeginInit();
             this.SuspendLayout();
             // 
             // ListLabel
             // 
             this.ListLabel.AutoSize = true;
-            this.ListLabel.Location = new System.Drawing.Point(14, 12);
+            this.ListLabel.Location = new System.Drawing.Point(12, 9);
             this.ListLabel.Name = "ListLabel";
-            this.ListLabel.Size = new System.Drawing.Size(53, 20);
+            this.ListLabel.Size = new System.Drawing.Size(43, 15);
             this.ListLabel.TabIndex = 0;
             this.ListLabel.Text = "Primes";
-            this.ListLabel.Click += new System.EventHandler(this.label_Click);
             // 
-            // CalculatePrimesParallel
+            // CalculatePrimesSequential
             // 
-            this.CalculatePrimesParallel.Location = new System.Drawing.Point(269, 73);
-            this.CalculatePrimesParallel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.CalculatePrimesParallel.Name = "CalculatePrimesParallel";
-            this.CalculatePrimesParallel.Size = new System.Drawing.Size(235, 31);
-            this.CalculatePrimesParallel.TabIndex = 1;
-            this.CalculatePrimesParallel.Text = "Calculate Primes sequential\r\n";
-            this.CalculatePrimesParallel.UseVisualStyleBackColor = true;
-            this.CalculatePrimesParallel.Click += new System.EventHandler(this.btnAdd_Click);
+            this.CalculatePrimesSequential.Location = new System.Drawing.Point(235, 122);
+            this.CalculatePrimesSequential.Name = "CalculatePrimesSequential";
+            this.CalculatePrimesSequential.Size = new System.Drawing.Size(206, 23);
+            this.CalculatePrimesSequential.TabIndex = 1;
+            this.CalculatePrimesSequential.Text = "Calculate Primes sequential\r\n";
+            this.CalculatePrimesSequential.UseVisualStyleBackColor = true;
+            this.CalculatePrimesSequential.Click += new System.EventHandler(this.btnGetPrimesSequential_Click);
             // 
             // PrimesList
             // 
             this.PrimesList.FormattingEnabled = true;
-            this.PrimesList.ItemHeight = 20;
-            this.PrimesList.Location = new System.Drawing.Point(14, 35);
-            this.PrimesList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PrimesList.ItemHeight = 15;
+            this.PrimesList.Location = new System.Drawing.Point(12, 26);
             this.PrimesList.Name = "PrimesList";
-            this.PrimesList.Size = new System.Drawing.Size(247, 464);
+            this.PrimesList.Size = new System.Drawing.Size(217, 349);
             this.PrimesList.TabIndex = 2;
-            // 
-            // fromInput
-            // 
-            this.fromInput.Location = new System.Drawing.Point(269, 35);
-            this.fromInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.fromInput.Name = "fromInput";
-            this.fromInput.Size = new System.Drawing.Size(114, 27);
-            this.fromInput.TabIndex = 3;
-            this.fromInput.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // FromLabel
             // 
             this.FromLabel.AutoSize = true;
-            this.FromLabel.Location = new System.Drawing.Point(269, 11);
+            this.FromLabel.Location = new System.Drawing.Point(235, 75);
             this.FromLabel.Name = "FromLabel";
-            this.FromLabel.Size = new System.Drawing.Size(43, 20);
+            this.FromLabel.Size = new System.Drawing.Size(35, 15);
             this.FromLabel.TabIndex = 4;
             this.FromLabel.Text = "From";
-            this.FromLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // toLabel
             // 
             this.toLabel.AutoSize = true;
-            this.toLabel.Location = new System.Drawing.Point(390, 11);
+            this.toLabel.Location = new System.Drawing.Point(361, 75);
             this.toLabel.Name = "toLabel";
-            this.toLabel.Size = new System.Drawing.Size(25, 20);
+            this.toLabel.Size = new System.Drawing.Size(19, 15);
             this.toLabel.TabIndex = 5;
             this.toLabel.Text = "To";
             // 
+            // CalculatePrimesParallel
+            // 
+            this.CalculatePrimesParallel.Location = new System.Drawing.Point(235, 151);
+            this.CalculatePrimesParallel.Name = "CalculatePrimesParallel";
+            this.CalculatePrimesParallel.Size = new System.Drawing.Size(206, 23);
+            this.CalculatePrimesParallel.TabIndex = 7;
+            this.CalculatePrimesParallel.Text = "Calculate Primes parallel";
+            this.CalculatePrimesParallel.UseVisualStyleBackColor = true;
+            this.CalculatePrimesParallel.Click += new System.EventHandler(this.btnGetPrimesParallel_Click);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(235, 44);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.ReadOnly = true;
+            this.timeTextBox.Size = new System.Drawing.Size(73, 23);
+            this.timeTextBox.TabIndex = 9;
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Location = new System.Drawing.Point(235, 26);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(33, 15);
+            this.TimerLabel.TabIndex = 8;
+            this.TimerLabel.Text = "Time";
+            // 
+            // fromInput
+            // 
+            this.fromInput.Location = new System.Drawing.Point(235, 93);
+            this.fromInput.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.fromInput.Name = "fromInput";
+            this.fromInput.Size = new System.Drawing.Size(120, 23);
+            this.fromInput.TabIndex = 10;
+            // 
             // toInput
             // 
-            this.toInput.Location = new System.Drawing.Point(390, 35);
-            this.toInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.toInput.Location = new System.Drawing.Point(361, 93);
+            this.toInput.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
             this.toInput.Name = "toInput";
-            this.toInput.Size = new System.Drawing.Size(114, 27);
-            this.toInput.TabIndex = 6;
+            this.toInput.Size = new System.Drawing.Size(120, 23);
+            this.toInput.TabIndex = 11;
             // 
             // Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(622, 508);
+            this.ClientSize = new System.Drawing.Size(555, 381);
             this.Controls.Add(this.toInput);
+            this.Controls.Add(this.fromInput);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.CalculatePrimesParallel);
             this.Controls.Add(this.toLabel);
             this.Controls.Add(this.FromLabel);
-            this.Controls.Add(this.fromInput);
             this.Controls.Add(this.PrimesList);
-            this.Controls.Add(this.CalculatePrimesParallel);
+            this.Controls.Add(this.CalculatePrimesSequential);
             this.Controls.Add(this.ListLabel);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form";
             this.Text = "PrimeGenerator";
             this.Click += new System.EventHandler(this.btnGetPrimesSequential_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.fromInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toInput)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,11 +167,14 @@
         #endregion
 
         private Label ListLabel;
-        private Button CalculatePrimesParallel;
+        private Button CalculatePrimesSequential;
         private ListBox PrimesList;
-        private TextBox fromInput;
         private Label FromLabel;
         private Label toLabel;
-        private TextBox toInput;
+        private Button CalculatePrimesParallel;
+        private TextBox timeTextBox;
+        private Label TimerLabel;
+        private NumericUpDown fromInput;
+        private NumericUpDown toInput;
     }
 }
--- ParallelProgramingCompulsory-Primes/Primes/PrimeGeneratorForm.cs
+++ ParallelProgramingCompulsory-Primes/Primes/PrimeGeneratorForm.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
 namespace Primes
 {
     public partial class Form : System.Windows.Forms.Form
@@ -10,39 +14,67 @@ namespace Primes
         private async void btnGetPrimesSequential_Click(object sender, EventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(fromInput.Text) && !string.IsNullOrWhiteSpace(toInput.Text))
-            { 
-                /*
+            {
+                //clear the display
+                this.timeTextBox.Text = "";
 
-            List<long> PrimesFound = await Task.Factory.StartNew(() => primeGenerator.GetPrimesSequential());
-                */
+                this.PrimesList.Items.Clear();
+
+                //Calculate results
+                long fromInputlong = long.Parse(fromInput.Text);
+                long toInputlong = long.Parse(toInput.Text);
+
+                Stopwatch sw = Stopwatch.StartNew();
+
+                List<long> primesFound = await Task.Factory.StartNew(() => primeGenerator.GetPrimesSequential(fromInputlong, toInputlong));
+
+                sw.Stop();
+
+                string time = String.Format("{0:f3} sec.", sw.Elapsed.TotalSeconds);
+
+                //
+                // Display the results:
+                //
+
+                this.PrimesList.BeginUpdate();
+
+                this.timeTextBox.Text = time;
+
+                foreach (var prime in primesFound)
+                {
+                    this.PrimesList.Items.Add(prime);
+                }
+
+                this.PrimesList.EndUpdate();
+
             }
 
         }
 
         private void btnGetPrimesParallel_Click(object sender, EventArgs e)
         {
-
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {/*
-            if (!string.IsNullOrWhiteSpace(fromInput.Text) && !PrimesList.Items.Contains(fromInput.Text))
-                PrimesList.Items.Add(fromInput.Text);*/
-        }
-
-        private void txtName_TextChanged(object sender, EventArgs e)
+        private Task<Boolean> DisplayAsync(string time, List<long> primesFound)
         {
+            return Task.Run<Boolean>(() => Display(time, primesFound));
 
         }
 
-        private void label_Click(object sender, EventArgs e)
+        private Boolean Display(string time, List<long> primesFound)
         {
+            this.timeTextBox.Text = time;
+
+            foreach (var prime in primesFound)
+            {
+                this.PrimesList.Items.Add(prime);
+            }
+
+            return true;
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
+
     }
 }
